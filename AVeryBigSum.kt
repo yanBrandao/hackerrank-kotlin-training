fa9b17f1a3eaeed7cc1c573ab373ca34d2ee38bd
Yan Tapajós <ybrandao.d@gmail.com>
2025-01-04 07:25:50 +0000

    Added new problems

---
+++ AVeryBigSum.kt
@@ -0,0 +1,25 @@
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+fun aVeryBigSum(ar: Array<Long>): Long {
+    // Write your code here
+    return ar.sum()
+}
+
+fun main(args: Array<String>) {
+    val arCount = readLine()!!.trim().toInt()
+
+    val ar = readLine()!!.trimEnd().split(" ").map{ it.toLong() }.toTypedArray()
+
+    val result = aVeryBigSum(ar)
+
+    println(result)
+}
---
+++ PlusMinus.kt
@@ -0,0 +1,48 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+fun plusMinus(arr: Array<Int>): Unit {
+    val values = hashMapOf<Int, Int>()
+
+    arr.forEach {
+        if (values.containsKey(it)) {
+            values[it] = values[it]!! + 1
+        } else {
+            values[it] = 1
+        }
+    }
+
+    values.forEach {
+        println(it.value / arr.size.toDouble())
+    }
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    plusMinus(arr)
+}
